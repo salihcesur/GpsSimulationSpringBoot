bd408835152326c4824ae25d2307941b0843ebd4
salihcesur <salihcesur72@gmail.com>
2024-09-11 10:01:46 +0300

    ProjectInitialised

---
+++ simulation/src/test/java/com/gps/simulation/SimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.simulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
